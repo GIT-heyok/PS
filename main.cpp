68b7ab4a866ec89fb48fe46094998e5ef7dcb1cd
GIT-heyok <122659622+GIT-heyok@users.noreply.github.com>
2023-01-17 13:53:20 +0000

    added tasks.json

--- main.cpp
+++ main.cpp
@@ -23,58 +23,43 @@ typedef vector<ll> vll;
 typedef pair<int, int> pI;
 typedef pair<ll, ll> pLL;
 const int MAX = 500001;
-const int INF = 1234567890;
+const ll INF = 1234567890ll;
 
-struct point2D{
-    ll x, y;
-    point2D(){
-        x = 0;
-        y = 0;
+bool isPalindrome(string& s, int i, int j){
+    while(i<j){
+        if(s[i]!=s[j]){
+            return false;
+        }
+        i++;
+        j--;
     }
-    point2D(ll a, ll b):x(a), y(b){};
-    
-    void debg(){
-        cout<<"x: "<<x<<" y: "<<y<<endl;
-    } 
-};
-
-struct vector2D{
-    point2D p1, p2;
-    ll x,y;
-    vector2D(point2D pp1, point2D pp2):p1(pp1), p2(pp2){
-        x = pp2.x-pp1.x;
-        y = pp2.y-pp1.y;
-    }
-    
-    vector2D(){
-        p1 = point2D();
-        p2 = point2D();
-        x= 0, y=0;
-    }
-    ld dist(){
-        return sqrt(x*x+y*y);
-    }
-};
-
-
-//-1 no 0 180 1 yes 
-int isCCW(point2D p1, point2D p2, point2D p3){
-    vector2D v1 = vector2D(p1, p2);
-    vector2D v2 = vector2D(p2, p3);
-    ll val = v1.x*v2.y-v2.x*v1.y;
-    if(val>0)return 1;
-    else if(val<0)return -1;
-    else return 0;
+    return true;
 }
-
-int main()
-{
+int memo[2501];
+int dp(string& s, int k){
+    if(memo[k]!=-1){
+        return memo[k];
+    }
+    else{
+        int& temp = memo[k] = INF;
+        if(isPalindrome(s, 0, k)){
+            temp = min(temp,1);
+        }
+        for(int i=0; i<k; i++){
+            if(isPalindrome(s, i+1, k)){
+                temp = min(temp, 1+dp(s, i));
+            }
+        }
+        return temp;
+    }
+   
+}
+int main(){
     FAST
-    int x1, y1;
-    int x2, y2;
-    int x3, y3;
-    cin >> x1>>y1;
-    cin >> x2>>y2;
-    cin >> x3>>y3;
-    cout<<isCCW(point2D(x1,y1),point2D(x2,y2),point2D(x3,y3))<<endl;
+    string s;
+    cin >> s;
+    for(int i=0; i<s.length(); i++){
+        memo[i] = -1;
+    }
+    cout<<dp(s, s.length()-1)<<endl;
 }
