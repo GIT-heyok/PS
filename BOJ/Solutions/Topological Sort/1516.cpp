17d431b993b0dca725588fe1372323ad372eab6d
hyeok2044 <hyeok2044@gmail.com>
2023-09-01 11:06:12 +0900

    solvings from 2023-09-01

--- BOJ/Solutions/Topological Sort/1516.cpp
+++ BOJ/Solutions/Topological Sort/1516.cpp
@@ -10,9 +10,5 @@ typedef long long ll;
 int main()
 {
     FAST
-    ll W, H, f, c, x1, y1, x2, y2;
-    cin >> W >> H >>f>> c>> x1>> y1>> x2>> y2;
-    ll M = f>W/2?W-f:f;
-    ll ans = W*H-(c+1)*(y2-y1)*(x2-x1+min(max(M-x1,0ll),x2-x1));
-    cout<<ans<<endl;
+    
 }
